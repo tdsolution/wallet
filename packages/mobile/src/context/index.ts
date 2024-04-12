6dfb83c921b7c807dbb5f3049396387e4f7f885a
DuongThiMinhNhu <19120163@at.hcmuaf.edu.vn>
2024-04-12 11:30:10 +0700

    fix bug

--- packages/mobile/src/context/index.ts
+++ packages/mobile/src/context/index.ts
@@ -1,3 +1,2 @@
 export * from './WalletContext';
 export * from './ChainContext';
-export * from './BalanceEvmContext';
--- packages/mobile/src/libs/EVM/token/tokenData.ts
+++ packages/mobile/src/libs/EVM/token/tokenData.ts
@@ -1,1 +1,208 @@
-// const 
+export const DataTokens : any[] = [
+    {
+       chainId :'1',
+       tokens: [
+        {
+            id:"weth", 
+            tokenAddress : "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            symbol : "WETH",
+            decimals: 18,
+            logo: "https://changenow.io/images/cached/weth.png",
+        },
+        {
+            id:"tether", 
+            tokenAddress : "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+        {
+            id:"shiba-inu", 
+            tokenAddress : "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
+            symbol : "SHIB",
+            decimals: 18,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png",
+        },
+    ]
+    },
+     {
+       chainId :'324',
+       tokens: [
+        {
+            id:"tether", 
+            tokenAddress : "0x493257fD37EDB34451f62EDf8D2a0C418852bA4C",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+     ]
+    },
+    {
+       chainId :'1116',
+       tokens: [
+         {
+            id:"wrapped-core", 
+            tokenAddress : "0x40375C92d9FAf44d2f9db9Bd9ba41a3317a2404f",
+            symbol : "USDT",
+            decimals: 18,
+            logo: "https://assets.coingecko.com/coins/images/29276/large/wcore.png?1696528228",
+        },
+        {
+            id:"coredaoorg", 
+            tokenAddress : "0xc24b642357d7dd1bbe33f3d8aa0101dfa2cf6eb9",
+            symbol : "CST",
+            decimals: 18,
+            logo: "https://i.imgur.com/r6mFFUu.jpg",
+        }
+     ]
+    },
+    {
+       chainId :'137',
+       tokens: [
+        {
+            id:"wmatic", 
+            tokenAddress : "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+            symbol : "WMATIC",
+            decimals: 18,
+            logo: "https://www.cryptocompare.com/media/43607694/wmatic.png",
+        },
+        {
+            id:"tether", 
+            tokenAddress : "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0x12016b4E07866c962e29b7597eCd66B3B89a3a58",
+            symbol : "TREND",
+            decimals: 6,
+            logo: "https://trendydefi.com/_next/image?url=%2Fimages%2Ftrendyloop.png&w=96&q=75",
+        },
+     ]
+    },
+   {
+       chainId :'10',
+       tokens: [
+         {
+            id:"tether", 
+            tokenAddress : "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+     ]
+    },
+    {
+       chainId :'42161',
+       tokens: [
+         {
+            id:"tether", 
+            tokenAddress : "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+     ]
+    },
+     {
+       chainId :'43114',
+       tokens: [
+         {
+            id:"tether", 
+            tokenAddress : "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+     ]
+    },
+     {
+       chainId :'38',
+       tokens: [
+        {
+            id:"wbnb", 
+            tokenAddress : "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
+            symbol : "WBNB",
+            decimals: 18,
+            logo: "https://app.plearnclub.com/images/tokens/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png",
+        },
+         {
+            id:"tether", 
+            tokenAddress : "0x55d398326f99059fF775485246999027B3197955",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+     ]
+    },
+    {
+       chainId :'97',
+       tokens: [
+         {
+            id:"wbnb", 
+            tokenAddress : "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
+            symbol : "WBNB",
+            decimals: 18,
+            logo: "https://app.plearnclub.com/images/tokens/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c.png",
+        },
+        {
+            id:"usd-coin", 
+            tokenAddress : "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+            symbol : "USDC",
+            decimals: 6,
+            logo: "https://s3.coinmarketcap.com/static-gravity/image/5a8229787b5e4c809b5914eef709b59a.png",
+        },
+         {
+            id:"tether", 
+            tokenAddress : "0x604e89Fd1F58460fB957C6b3bEf9413fEd9cd229",
+            symbol : "USDT",
+            decimals: 6,
+            logo: "https://s2.coinmarketcap.com/static/img/coins/200x200/825.png",
+        },
+     ]
+    },
+]
