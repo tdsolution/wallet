c43fcb3dc35e6179235a70e7987bae5fa61af337
CamThuy0212 <camthuy0212@gmail.com>
2024-04-23 15:03:58 +0700

    add check explorer

--- packages/mobile/src/screens/DetailToken/DetailToken.tsx
+++ packages/mobile/src/screens/DetailToken/DetailToken.tsx
@@ -17,14 +17,15 @@ import { trackEvent } from "$utils/stats";
 import { Events, SendAnalyticsFrom } from "$store/models";
 import { useFlags } from "$utils/flags";
 import { t } from "@tonkeeper/shared/i18n";
-import { useChain, useWallet } from "@tonkeeper/shared/hooks";
-import { WalletStackRouteNames } from "$navigation";
+import { useChain, useEvm, useWallet } from "@tonkeeper/shared/hooks";
+import { WalletStackRouteNames, openDAppBrowser } from "$navigation";
 
 
 const DetailToken = ({ route }: any) => {
   const { id, symbol, image, address, addressToken, rpc } = route.params;
   const navigation = useNavigation();
   const chain = useChain()?.chain;
+  const evm = useEvm()?.evm;
   const handleBack = () => {
     navigation.goBack();
   };
@@ -66,6 +67,44 @@ const DetailToken = ({ route }: any) => {
       openRequireWalletModal();
     }
   }, [navigation, wallet]);
+
+  const buildTransactionUrl = (address: string, blockchainType: string): string => {
+    switch (blockchainType) {
+      case '1':
+        return `https://etherscan.io/address/${address}`;
+      case '324':
+        return `https://explorer.zksync.io/address/${address}`;
+      case '56':
+        return `https://bscscan.com/address/${address}`;
+      case '1116':
+        return `https://scan.coredao.org/address/${address}`;
+      case '97':
+        return `https://polygonscan.com/address/${address}`;
+      case '10':
+        return `https://optimistic.etherscan.io/address/${address}`;
+      case '137':
+        return `https://polygonscan.com/address/${address}`;
+      // case '1000':
+      //   return `https://tronscan.org/#/address/${AppDataGlobal.ADDRESS_TRON}`;
+      // case '1100':
+      //   return `https://tonscan.org/address/${AppDataGlobal.ADDRESS_TON}`;
+      case '43114':
+        return `https://avascan.info/blockchain/dfk/address/${address}/transactions`;
+      case '42161':
+        return `https://arbiscan.io/address/${address}`;
+      case '250':
+        return `https://ftmscan.com/address/${address}`;
+      case '1088':
+        return `https://explorer.metis.io/address/${address}`;
+      case '42220':
+        return `https://celoscan.io/address/${address}`;
+      case '25':
+        return `https://cronoscan.com/address/${address}`;
+      default:
+        throw new Error('Invalid blockchain type');
+    }
+  };
+
   return (
     <SafeAreaView>
       <View
@@ -80,8 +119,9 @@ const DetailToken = ({ route }: any) => {
             source={require("../../assets/icons/png/ic-arrow-up-16.png")}
           />
         </TouchableOpacity>
-        <Text style={globalStyles.textHeader}>{chain.name}</Text>
-        <Text style={{ opacity: 0 }}>scacasc</Text>
+        <View style={{width: "100%", alignItems: "center"}}>
+        <Text style={[globalStyles.textHeader, {marginLeft: -45}]}>{chain.name}</Text>
+        </View>
       </View>
       <View style={{ justifyContent: "center", alignItems: "center" }}>
         {image ? (
@@ -158,7 +198,7 @@ const DetailToken = ({ route }: any) => {
         }}
       >
         <Text style={styles.caption}>Can't find your transaction? </Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => openDAppBrowser(buildTransactionUrl(evm.addressWallet, chain.chainId))}>
           <Text style={styles.textCheckExplorer}>Check explorer</Text>
         </TouchableOpacity>
       </View>
