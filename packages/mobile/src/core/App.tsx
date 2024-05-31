bf64f059cc59273ec9b596ef96153bb28bd948b1
DuongThiMinhNhu <19120163@at.hcmuaf.edu.vn>
2024-05-31 11:08:44 +0700

    fix bug

--- packages/mobile/src/core/App.tsx
+++ packages/mobile/src/core/App.tsx
@@ -17,7 +17,7 @@ import { isAndroid } from "$utils";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { HideableAmountProvider } from "$core/HideableAmount/HideableAmountProvider";
 import { Core } from '@walletconnect/core'
-import { Web3Wallet } from '@walletconnect/web3wallet'
+import { Web3Wallet } from '@walletconnect/web3wallet';
 import { queryClient } from "@tonkeeper/shared/queryClient";
 import {
   ChainProvider,
@@ -28,21 +28,7 @@ import {
 } from "../context";
 import { BlockingLoaderView } from "@tonkeeper/uikit";
 import { SwapCoinProvider } from "../context/SwapCoinContext";
-  const core = new Core({
-  projectId: process.env.PROJECT_ID
-});
-const web3wallet = await Web3Wallet.init({
-  core, // <- pass the shared `core` instance
-  metadata: {
-    name: 'Demo React Native Wallet',
-    description: 'Demo RN Wallet to interface with Dapps',
-    url: 'www.walletconnect.com',
-    icons: ['https://your_wallet_icon.png'],
-    redirect: {
-      native: 'yourwalletscheme://'
-    }
-  }
-})
+
 const TonThemeProvider = ({ children }) => {
 
   const accent = useSelector(accentSelector);
